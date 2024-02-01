cc823535fdfc46c7e3b2e03faf45c1b1077923b5
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-02-01 12:27:42 +0100

    rock paper scissors

---
+++ rockpaperscissors/script.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const playerChoices = document.querySelector ('.rock, .paper, .scissors');
+    const player1 = document.getElementById('player1');
+    const player2 = document.getElementById('player2');
+    const winText = document.getElementById('win');
+    const loseText = document.getElementById('lose');
+    const drawText = document.getElementById('draw');
+  
+    playerChoices.forEach(choice => {
+      choice.addEventListener('click', function() {
+        const playerChoice = choice.classList[0]; 
+        playRound(playerChoice);
+      });
+    });
+  
+    function playRound(playerChoice) {
+      const choices = ['rock', 'paper', 'scissors'];
+      const computerChoice = choices[Math.floor(Math.random() * 3)];
+  
+      player1.className = `player ${playerChoice}`;
+      player2.className = `player ${computerChoice}`;
+  
+      showResult(playerChoice, computerChoice);
+    }
+  
+    function showResult(playerChoice, computerChoice) {
+      winText.classList.add('hidden');
+      loseText.classList.add('hidden');
+      drawText.classList.add('hidden');
+  
+      if (playerChoice === computerChoice) {
+        drawText.classList.remove('hidden');
+      } else if (
+        (playerChoice === 'rock' && computerChoice === 'scissors') ||
+        (playerChoice === 'paper' && computerChoice === 'rock') ||
+        (playerChoice === 'scissors' && computerChoice === 'paper')
+      ) {
+        winText.classList.remove('hidden');
+      } else {
+        loseText.classList.remove('hidden');
+      }
+    }
+  });
+  
