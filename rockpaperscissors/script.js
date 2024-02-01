fa9574490512ffb109596533817aaebd4800c8a3
Andrea <132267442+andreahauberg@users.noreply.github.com>
2024-02-01 13:28:53 +0100

    all

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const playerChoices = document.querySelector ('.rock, .paper, .scissors');
+    const playerChoices = document.querySelectorAll ('.rock, .paper, .scissors');
     const player1 = document.getElementById('player1');
     const player2 = document.getElementById('player2');
     const winText = document.getElementById('win');
